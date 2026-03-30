960024f956d1bde34cb039807c7b2b9be1ace1a6
Robin Escobar <rescobar@buildwithin.com>
2026-03-29 22:37:38 -0600

    fix migration

--- sabor-que-viaja/db/knexfile.ts
+++ sabor-que-viaja/db/knexfile.ts
@@ -1,15 +1,18 @@
 import type { Knex } from "knex";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "better-sqlite3",
     connection: {
-      filename: path.resolve(process.cwd(), "db/sabor-que-viaja.sqlite"),
+      filename: path.resolve(__dirname, "sabor-que-viaja.sqlite"),
     },
     useNullAsDefault: true,
     migrations: {
-      directory: path.resolve(process.cwd(), "db/migrations"),
+      directory: path.resolve(__dirname, "migrations"),
       extension: "ts",
     },
   },
